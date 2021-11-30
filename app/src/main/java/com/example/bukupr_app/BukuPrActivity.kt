c598fc901c3757a1ea74a42ddc08c19c838f3062
druchiat <62007520+druchiat@users.noreply.github.com>
2021-11-30 16:15:26 +0700

    Progress 1 Layout Halaman Buku PR

---
+++ app/src/main/java/com/example/bukupr_app/BukuPrActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bukupr_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BukuPrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buku_pr)
+    }
+}
