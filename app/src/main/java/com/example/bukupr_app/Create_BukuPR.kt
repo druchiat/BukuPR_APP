75a36fd72ead0259329fd3863e4069ae28f7c879
andikarakam <andikarakam@gmail.com>
2021-12-02 22:42:58 +0700

    dikhead

---
+++ app/src/main/java/com/example/bukupr_app/Create_BukuPR.kt
@@ -0,0 +1,11 @@
+package com.example.bukupr_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Create_BukuPR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_buku_pr)
+    }
+}
